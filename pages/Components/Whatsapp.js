1c21fa54898ca3ec44de41e154aabeee913cca22
Van Jordan <39660749+vanman9599@users.noreply.github.com>
2024-05-30 12:55:45 -0700

    Update Whatsapp.js

--- pages/Components/Whatsapp.js
+++ pages/Components/Whatsapp.js
@@ -4,7 +4,7 @@ export default function Whatsapp() {
     return (<div className='relative'>
         <div className='  fixed bottom-20 right-20 hidden md:block '>
             <button className='outline outline-2 outline-offset-2 outline-cyan-400 rounded-full py-5 px-10 md:text-xl mx-2 my-10 bg-cyan-500 '>
-                <Link href='https://wa.me/+6281274888687'>call us on whatsapp </Link></button>
+                <Link href='https://wa.me/+6281274888687'>Call us on WhatsApp </Link></button>
         </div>
     </div>
     )
