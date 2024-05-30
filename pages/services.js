c35441da0901a0a2d294d625cc5183b9bb9ab85b
Van Jordan <39660749+vanman9599@users.noreply.github.com>
2024-05-30 10:24:25 -0700

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -29,7 +29,7 @@ export default function Services() {
                     <p className='text-wrap '>We do IV Drip Treatment for: Dehydration, <br />
                         Bali belly, Fatigue, Dengue Fever, Hangover, and Immune booster. <br />See our menu below</p>
                     <button className='outline outline-2 outline-offset-2 outline-cyan-400 rounded-full py-5 px-10 md:text-xl mx-2 my-10  '>
-                        <a href='https://wa.me/+6281274888687'>BE IN TOUCH </a></button>
+                        <a href='https://wa.me/+6281274888687'>GET IN TOUCH </a></button>
                 </div>
             </div>
             <Whatsapp />
@@ -47,7 +47,7 @@ export default function Services() {
                 <div className='pt-6 pl-3'>
                     <p className='text-wrap'>We treat all wounds from small to large. <br />Our careful nurses will have you healed in no time!</p>
                     <button className='outline outline-2 outline-offset-2 outline-cyan-400 rounded-full py-5 px-10 md:text-xl mx-2 my-10  '>
-                        <Link href='https://wa.me/+6281274888687'>BE IN TOUCH </Link></button>
+                        <Link href='https://wa.me/+6281274888687'>GET IN TOUCH </Link></button>
                 </div>
                 <Image
                     alt="Mountains"
@@ -75,7 +75,7 @@ export default function Services() {
                 <div className='pt-10 px-5' >
                     <p className='text-wrap '>We offer a full range of postpartum treatment <br />including bathing and care of umbilical cord, <br />baby massage, postpartum breast care.</p>
                     <button className='outline outline-2 outline-offset-2 outline-cyan-400 rounded-full py-5 px-10 md:text-xl mx-2 my-10  '>
-                        <Link href='https://wa.me/+6281274888687'>BE IN TOUCH </Link></button>
+                        <Link href='https://wa.me/+6281274888687'>CALL NOW </Link></button>
                 </div>
             </div>
 
@@ -92,7 +92,7 @@ export default function Services() {
                 <div className='pt-10 px-5'>
                     <p className='text-wrap'>Consult with our staff General Practictioner  via Whatsapp. <br />Please download and complete Medical <br />Consent Form prior to consultation.</p>
                     <button className='outline outline-2 outline-offset-2 outline-cyan-400 rounded-full py-5 px-10 md:text-xl mx-2 my-10  '>
-                        <Link href='https://wa.me/+6281274888687'>BE IN TOUCH </Link></button>
+                        <Link href='https://wa.me/+6281274888687'>CALL NOW </Link></button>
                 </div>
                 <Image
                     alt="Mountains"
